dd724a433990a9075e35213c95a95efcb29fc8b2
Anthony Gavidia-Vasquez <agavivasq@gmail.com>
2025-03-24 15:58:50 -0400

    made expense id layout

--- src/app/dashboard/expenses/[id]/page.tsx
+++ src/app/dashboard/expenses/[id]/page.tsx
@@ -4,7 +4,7 @@ import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 export default function DetailPage() {
     return (
         <div className="min-h-screen bg-white flex flex-col gap-10">
-            <article className="flex flex-col px-3 pt-6 pb-10 space-y-6">
+            <article className="flex flex-col px-3 pt-10 pb-3 space-y-8">
                 <section className="flex justify-between items-center md:hidden">
                     <h1 className="text-2xl sm:text-3xl md:text-5xl underline">Expense Name</h1>
                     <section className="flex gap-5 items-center">
@@ -28,34 +28,33 @@ export default function DetailPage() {
                                 <button className="w-8 h-8"><TrashIcon/></button>
                             </section>
                         </section>
-                        <section className="flex flex-col">
-                            <h2 className="text-xl sm:text-3xl underline mb-3">Expense Details:</h2>
-                            <section className="grid grid-cols-2 gap-5 text-white font-bold text-sm">
-                                <ul className="col-span-1 h-full space-y-5">
-                                    <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 1</li>
-                                    <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 2</li>
-                                    <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 3</li>
-                                </ul>
-                                <ul className="col-span-1 h-full space-y-5">
-                                    <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 4</li>
-                                    <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 5</li>
-                                    <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 6</li>
-                                </ul>
-                            </section>
+                        <section className="grid grid-cols-2 gap-5 text-white font-bold text-sm">
+                            <ul className="col-span-1 h-full space-y-5">
+                                <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 1</li>
+                                <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 2</li>
+                                <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 3</li>
+                            </ul>
+                            <ul className="col-span-1 h-full space-y-5">
+                                <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 4</li>
+                                <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 5</li>
+                                <li className="px-3 py-6 bg-indigo-500 rounded-lg">Item 6</li>
+                            </ul>
                         </section>
                     </section>
                 </section>
-                <section>
-                    <h2 className="text-xl sm:text-xl underline">Expense Description:</h2>
-                    <p className="text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, commodi exercitationem! Debitis, porro ad earum, 
-                        aut dolores dolore velit labore praesentium eligendi optio quod, rerum deserunt sint voluptatibus saepe iusto.
-                    </p>
-                </section>
-                <section>
-                    <h2 className="text-xl sm:text-xl underline">Additional Notes:</h2>
-                    <p className="text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, commodi exercitationem! Debitis, porro ad earum, 
-                        aut dolores dolore velit labore praesentium eligendi optio quod, rerum deserunt sint voluptatibus saepe iusto.
-                    </p>
+                <section className=" flex flex-col md:grid sm:grid-cols-2 gap-5 pb-10">
+                    <section>
+                        <h2 className="text-xl sm:text-3xl underline">Expense Description:</h2>
+                        <p className="text-sm sm:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, commodi exercitationem! Debitis, porro ad earum, 
+                            aut dolores dolore velit labore praesentium eligendi optio quod, rerum deserunt sint voluptatibus saepe iusto.
+                        </p>
+                    </section>
+                    <section>
+                        <h2 className="text-xl sm:text-3xl underline">Additional Notes:</h2>
+                        <p className="text-sm sm:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, commodi exercitationem! Debitis, porro ad earum, 
+                            aut dolores dolore velit labore praesentium eligendi optio quod, rerum deserunt sint voluptatibus saepe iusto.
+                        </p>
+                    </section>
                 </section>
             </article>
         </div>
