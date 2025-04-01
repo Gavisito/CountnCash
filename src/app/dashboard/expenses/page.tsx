d0c25d1f12e783fb8305e9197f1a805ca8079c00
Anthony Gavidia-Vasquez <agavivasq@gmail.com>
2025-04-01 13:32:09 -0400

    put some error logging

--- src/app/dashboard/expenses/page.tsx
+++ src/app/dashboard/expenses/page.tsx
@@ -2,6 +2,7 @@ import { Expense } from "@/app/types/expense";
 import ExpenseListing from "@/app/components/listings/expenseListing";
 import Link from "next/link";
 import { TableCellsIcon, QueueListIcon, Squares2X2Icon, PlusCircleIcon } from "@heroicons/react/24/outline"; 
+import { notFound } from "next/navigation";
 // notes section
 // refactored the code so i get a habit of maing server side rendering, especially for api data and so if the data does not loading up there is a fail safe compoennt when it doesnt load properly
 // this helped me undertsand when to use client or server side rendering a lot. 
@@ -11,6 +12,11 @@ export default async function ExpensePage() {
         const baseUrl = process.env.NEXT_PUBLIC_APP_URL || '';
         // getting geenral expense data
         const response = await fetch(`${baseUrl}/api/expenses`,{ cache: 'no-store'});
+
+        if (!response.ok){
+            throw new Error("Error fetching data");
+            notFound()
+        }
 
         // storing data into one variable for better readability
         const jsondata = await response.json();
@@ -26,21 +32,17 @@ export default async function ExpensePage() {
             </div>
         );
 
-    } catch {
+    } catch (error) {
+        console.log(error);
         return (
             <div className="h-full flex flex-col p-3 gap-6 mt-10">
                 {/*Expense List Page Title*/}
                 <section className="flex flex-row w-full">
                     <h1 className="mt-6 mb-2 text-4xl font-bold text-indigo-950">Expense List</h1>
-                    <button className="w-full"> 
-                        <Link href="/dashboard/expenses/">
-                            <PlusCircleIcon className="w-5 h-5 bg-black"/>
-                        </Link>
-                    </button>
                 </section>
                 <article className="flex flex-col gap-5">
                     {/*Layout Choices To Displaying Content*/}
-                    <section className="flex flex-row gap-5">
+                    <section className="flex flex-col gap-5">
                         {/*Layout Choices To Displaying Content*/}
                         <section className="flex flex-row gap-5">
                             {/*Display Table Button*/}
@@ -61,13 +63,11 @@ export default async function ExpensePage() {
                                     <Squares2X2Icon/>
                                 </button>
                             </div>
-                            <div>
-                                <button className="rounded-sm text-sm mt-3 hover:cursor-pointer">
-                                    <Link className="justify-center gap-2 items-center flex flex-row" href="/dashboard/expense/">
-                                        <PlusCircleIcon className="w-10 h-10"/> 
-                                    </Link>
-                                </button>
-                            </div>
+                            <button className="rounded-sm text-sm hover:cursor-pointer">
+                                <Link className="" href="/dashboard/expense/">
+                                    <PlusCircleIcon className="w-10 h-10"/> 
+                                </Link>
+                            </button>
                         </section>
                         <section>
                             <p>There was an error loading up the your expenses...</p>
